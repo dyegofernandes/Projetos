eecd1cff4f038d334737da05d0e8cfb39d20c98c
Juniel Alves da Silva <juniel.silva@ati.pi.gov.br>
2018-06-10 18:21:16 -0300

    commit

---
+++ Pessoal/testebbBoletos/src/main/java/carnepagcontas/com/tibco/schemas/bws_registro_cbr/recursos/xsd/schema/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.tibco.com/schemas/bws_registro_cbr/Recursos/XSD/Schema.xsd", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package carnepagcontas.com.tibco.schemas.bws_registro_cbr.recursos.xsd.schema;
